28f8b52b60db1120c5ab1fcc5beafcc4d7823d30
tmarktaylor <24257134+tmarktaylor@users.noreply.github.com>
2020-03-18 14:14:32 -0400

    delete unsupported 'Topic :: Software Development :: Test Tools'. bump release version to 0.0.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',  # Required
+    version='0.0.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -89,7 +89,6 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Test Tools',
         "Topic :: Software Development :: Testing",
         "Topic :: Software Development :: Documentation",
         'Topic :: Software Development :: Libraries :: Python Modules',
