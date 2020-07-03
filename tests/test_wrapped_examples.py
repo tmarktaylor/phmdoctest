b39be6f5a9c97898690b71750b941f2b3991bdf6
tmarktaylor <24257134+tmarktaylor@users.noreply.github.com>
2020-07-03 06:59:44 -0400

    Add test case for doc/test_example3_py.md.
    o add missing encoding='utf-8'.

--- tests/test_wrapped_examples.py
+++ tests/test_wrapped_examples.py
@@ -6,11 +6,17 @@ import verify
 
 def test_test_example2_py_md():
     """The copy of .py file in fenced code block is the same as the file."""
-    with open('doc/test_example2.py', 'r') as f:
+    with open('doc/test_example2.py', 'r', encoding='utf-8') as f:
         want = f.read()
     blocks = phmdoctest.tool.fenced_code_blocks('doc/test_example2_py.md')
     got = blocks[0]
     verify.a_and_b_are_the_same(a=want, b=got)
 
 
-
+def test_test_example3_py_md():
+    """The copy of .py file in fenced code block is the same as the file."""
+    with open('doc/test_example3.py', 'r', encoding='utf-8') as f:
+        want = f.read()
+    blocks = phmdoctest.tool.fenced_code_blocks('doc/test_example3_py.md')
+    got = blocks[0]
+    verify.a_and_b_are_the_same(a=want, b=got)
